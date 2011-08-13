5a89334505c62cdc5c226322f0ec5827d27e0404
Greg Shackles <gshackles@gmail.com>
2011-08-13 10:44:59 -0400

    initial Nancy commit

---
+++ Modules/MainModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace CodeCampService.Modules
+{
+    public class MainModule : NancyModule
+    {
+        public MainModule()
+        {
+            Get["/"] = parameters => "o hai";
+        }
+    }
+}
