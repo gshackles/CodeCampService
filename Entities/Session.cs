eb5c75ef3cade9721fd391561c351033af52b1fc
Greg Shackles <gshackles@gmail.com>
2011-08-16 10:48:08 -0400

    setting the Tag element name during serialization

--- Entities/Session.cs
+++ Entities/Session.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 
 namespace CodeCampService
 {
@@ -11,6 +12,8 @@ namespace CodeCampService
 		public string Speaker { get; set; }
 		public DateTime StartDate { get; set; }
 		public DateTime EndDate { get; set; }
+		
+		[XmlArrayItem("Tag")]
 		public List<string> Tags { get; set; }
 	}
 }
